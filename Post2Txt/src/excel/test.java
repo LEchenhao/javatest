02dc73c4148a164d940c9c74ad17c0ad9f44bde5
LECH-Lab <LECH-Lab@DESKTOP-J3G1N2G>
2017-10-23 16:15:46 +0800

    new

--- Post2Txt/src/excel/test.java
+++ Post2Txt/src/excel/test.java
@@ -1,3 +1,24 @@
 import new
 hah
 test
+
+for (short columnIndex = 0; columnIndex <= row.getLastCellNum(); columnIndex++) {
+
+    String value = "";
+
+    cell = row.getCell(columnIndex);
+
+    if (cell != null) {
+
+       // 注意：一定要设成这个，否则可能会出现乱码
+
+       //cell.setEncoding(HSSFCell.ENCODING_UTF_16);
+  	 // cell.setEncoding(HSSFCell.ENCODING_GBK);
+
+       switch (cell.getCellType()) {
+
+       case HSSFCell.CELL_TYPE_STRING:
+
+           value = cell.getStringCellValue();
+
+           break;
